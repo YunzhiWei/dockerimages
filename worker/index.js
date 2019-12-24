29b77a5a68c551a78978783a355767fecea003f1
Chris Wei <yunzhi.wei@live.com>
2019-12-24 17:28:47 +0800

    feat (worker): access database

--- worker/index.js
+++ worker/index.js
@@ -1,1 +1,20 @@
-console.log('hello world');
+const libKnex = require('knex')({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    database : 'docker',
+    user : 'docker',
+    password : '123456',
+  }
+});
+
+const TABLE_NAME = 'dbt_maindata';
+
+async function main () {
+    const records = await libKnex(TABLE_NAME).select();
+    console.log(`length: ${records.length}`);
+    console.log(`records: `, records);
+    return records;
+}
+
+main();
