fd4cdbce73b4823f365d8b0dcd0936d8638050c6
Chris Wei <yunzhi.wei@live.com>
2019-12-24 16:46:31 +0800

    feat (database): create table and seeds records

--- database/docker-entrypoint-initdb.d/initial.sh
+++ database/docker-entrypoint-initdb.d/initial.sh
@@ -14,5 +14,20 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname docker <<-EOSQL
 EOSQL
 
 psql -v ON_ERROR_STOP=1 --username docker --dbname docker <<-EOSQL
-    \i maindata.pg;
+
+CREATE TABLE dbt_maindata (
+    id                  UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    ex_info             jsonb,
+    tree_path	        ltree,
+    name                text
+);
+CREATE INDEX idx_maindata_path_gist  ON dbt_maindata USING GIST (tree_path);
+CREATE INDEX idx_maindata_path_idx   ON dbt_maindata USING BTREE (tree_path);
+
+
+INSERT INTO dbt_maindata (tree_path, name) VALUES
+    ('A',   '首页看板'),
+    ('A.B', '内容管理')
+;
+
 EOSQL
