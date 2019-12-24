43e6f4d162d4155f387e5b2cf5f811efc949090c
Chris Wei <yunzhi.wei@live.com>
2019-12-24 18:28:35 +0800

    feat (service & env): add package.json in root folder and move db.js into lib folder

--- service/src/app.service.ts
+++ service/src/app.service.ts
@@ -1,11 +1,11 @@
 import { Injectable } from '@nestjs/common';
-import { libKnex } from './db';
+import { libKnex } from './lib/db';
 
 const TABLE_NAME = 'dbt_maindata';
 
 @Injectable()
 export class AppService {
-  async getHello(): string {
+  async getHello() {
     const records = await libKnex(TABLE_NAME).select();
     return `size: ${records.length}, name[0]: ${records[0].name}`;
   }
