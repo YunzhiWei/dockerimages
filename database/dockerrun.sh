0336924e5c667db6d8e5938c77fae86e6823af3f
Chris Wei <yunzhi.wei@live.com>
2019-12-24 18:38:30 +0800

    fix (env): to enable script build and start

--- database/dockerrun.sh
+++ database/dockerrun.sh
@@ -4,4 +4,4 @@
 docker run --name dbpg -e POSTGRES_PASSWORD=123456 -p 5432:5432 -v "$PWD"/docker-entrypoint-initdb.d:/docker-entrypoint-initdb.d -d postgres:alpine
 
 #execute bash in container
-docker exec -it dbpg /bin/bash
+#docker exec -it dbpg /bin/bash
