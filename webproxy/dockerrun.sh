c49fba2234a3aefb28934b7860cb54bfb093bd33
Chris Wei <yunzhi.wei@live.com>
2020-01-09 14:57:21 +0800

    fix (devops): parameters

--- webproxy/dockerrun.sh
+++ webproxy/dockerrun.sh
@@ -3,7 +3,7 @@
 #start proxy with image --restart always 
 # Bug: !!! docker run --name proxy -v "$PWD"/www:/www -v "$PWD"/etc/nginx:/etc/nginx/ -p 8000:80 -d nginx:1.17.6-alpine
 # Bug: !!! docker run --name proxy -v "$PWD"/www:/www -p 8000:80 -d nginx:1.17.6-alpine
-docker run --name proxy --network bridgenet -p 80:80 -d chriswei/simpleproxy
+docker run --name proxy -p 80:80 -d chriswei/simpleproxy:0.0.1
 
 # 
 # docker exec proxy ls /etc/nginx
