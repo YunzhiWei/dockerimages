c154a5803af5f77c4bc1e3769f5b1813ddcb3edb
Chris Wei <yunzhi.wei@live.com>
2020-01-27 12:45:48 +0800

    fix (name): use new name of 'restapi'

--- service/dockerrun.sh
+++ service/dockerrun.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #start service api with its image
 
-docker run --network bridgenet --name service -p 3000:3000 -d chriswei/restapi
+docker run --network bridgenet --name restapi -p 3000:3000 -d chriswei/restapi
